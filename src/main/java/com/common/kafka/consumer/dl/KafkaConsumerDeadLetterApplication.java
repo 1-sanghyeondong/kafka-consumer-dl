7df5e1fb5f6947de4e57eab049e2625551168b7c
carl.up1 <carl.up1@kakaocorp.com>
2026-02-06 09:11:31 +0900

    Init

--- src/main/java/com/common/kafka/consumer/dl/KafkaConsumerDeadLetterApplication.java
+++ src/main/java/com/common/kafka/consumer/dl/KafkaConsumerDeadLetterApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class KafkaConsumerDeadLetterApplication {
     public static void main(String[] args) {
-        // ✅ 클래스 이름 일치시킴
         SpringApplication.run(KafkaConsumerDeadLetterApplication.class, args);
     }
 }
